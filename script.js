9ed2316410c99a82ba722bd2f8a69e0f6993cd21
Nimira43 <n.rabone72@aol.com>
2024-12-25 11:24:05 +0000

    Positioned the interior correctly.

--- script.js
+++ script.js
@@ -4,16 +4,14 @@ function enterTardis() {
   
   exterior.style.visibility = 'hidden'
   interior.style.visibility = 'visible'
-  interior.style.transform = 'rotateY(0deg)'
 }
 
 function exitTardis() {
   const exterior = document.getElementById('exterior')
   const interior = document.getElementById('interior')
 
-  interior.style.transform = 'rotateY(180deg)'
+  interior.style.visibility = 'hidden'
   setTimeout(() => {
-    interior.style.visibility = 'hidden'
     exterior.style.visibility = 'visible'
   }, 1000)
 }
