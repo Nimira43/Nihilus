170d1a25a1c5c514f4d13e9c1beae4cdc60dc195
Nimira43 <n.rabone72@aol.com>
2024-12-25 10:36:31 +0000

    Correct typo in CSS. Wrote JS script to show a simple transition of entering the TARDIS and exiting.

---
+++ script.js
@@ -0,0 +1,9 @@
+function enterTardis() {
+  document.getElementById('exterior').style.visibility = 'hidden';
+  document.getElementById('interior').style.visibility = 'visible';
+}
+
+function exitTardis() {
+  document.getElementById('interior').style.visibility = 'hidden';
+  document.getElementById('exterior').style.visibility = 'visible';
+}
