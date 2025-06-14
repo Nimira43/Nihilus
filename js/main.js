8d39f1e8a6a96a93cae8b9aa395dc26df1cfa25d
Nimira43 <n.rabone72@aol.com>
2025-06-14 13:53:06 +0100

    Mouse circle function

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const mouseCircle = document.querySelector('.mouse-circle')
+const mouseDot = document.querySelector('.mouse-dot')
+
+let mouseCircleBool = true
+
+const mouseCircleFn = (x, y) => {
+  mouseCircleBool && (
+    mouseCircle.style.cssText = `
+      top: ${y}px; left: ${x}px; opacity: 1
+    `
+  )
+
+  mouseDot.style.cssText = `
+      top: ${y}px; left: ${x}px; opacity: 1
+    `
+}
