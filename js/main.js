f2b718ece8c0e9ee72ff42f745ba660a007b1542
Nimira43 <n.rabone72@aol.com>
2025-06-14 17:52:18 +0100

    main btn JS - 2

--- js/main.js
+++ js/main.js
@@ -127,4 +127,14 @@ const mainBtns = document.querySelectorAll('.main-btn')
 
 mainBtns.forEach((btn) => {
   let ripple
+
+  btn.addEventListener('mouseenter', (e) => {
+    const left = e.clientX = e.target.getBoundingClientRect().left
+    const top = e.clientY = e.target.getBoundingClientRect().top
+  })
+
+  ripple = document.createElement('div')
+  ripple.classList('ripple')
+  ripple.style.left = `${left}px`
+  ripple.style.top = `${top}px`
 })
