2127e857ab0890911f5042e30caecc51f0952f8a
Nimira43 <n.rabone72@aol.com>
2025-06-14 14:26:44 +0100

    Starting to create mouseCircleTransform function

--- js/main.js
+++ js/main.js
@@ -14,3 +14,25 @@ const mouseCircleFn = (x, y) => {
       top: ${y}px; left: ${x}px; opacity: 1
     `
 }
+
+const mouseCircleTransform = (hoveredEl) => {
+  if (hoveredEl.classList.contains ('pointer-event')) {
+    hoveredEl.onmousemove = () => {
+      mouseCircleBool = false
+      mouseCircle.style.cssText = `
+        width: ${hoveredEl.getBoundingClientRect().width}px
+        height: ${hoveredEl.getBoundingClientRect().height}px
+        top: ${hoveredEl.getBoundingClientRect().top}px
+        left: ${hoveredEl.getBoundingClientRect().left}px
+        opacity: 1
+        transform: translate(0, 0)
+        animation:none;
+        border-radius: ${getComputedStyle(hoveredEl).borderBottomLeftRadius}
+        transition: width 0.5s, height 0.5s, top: 0.5s. left 0.5s, transform 0.5s, border-radius 0.5s
+      `
+    }
+    hoveredEl.onmouseleave = () => {
+      mouseCircleBool = true
+    }
+  }
+}
